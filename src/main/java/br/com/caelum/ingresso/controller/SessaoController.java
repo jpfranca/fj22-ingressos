7c63c34ac2e743eb1cfe6c552815f3716ece2e98
João França <joao.franca@outlook.com.br>
2020-12-06 23:46:29 -0300

    Exercicio 2.9 pt2

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -5,6 +5,7 @@ import br.com.caelum.ingresso.dao.SalaDao;
 import br.com.caelum.ingresso.dao.SessaoDao;
 import br.com.caelum.ingresso.model.Sessao;
 import br.com.caelum.ingresso.model.form.SessaoForm;
+import br.com.caelum.ingresso.validacao.GerenciadorDeSessao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @Controller
 public class SessaoController {
@@ -37,9 +39,19 @@ public class SessaoController {
     @PostMapping(value = "/admin/sessao")
     @Transactional
     public ModelAndView salva(@Valid SessaoForm form, BindingResult result) {
-        if (result.hasErrors()) return form(form.getSalaId(),form);
+
+        if (result.hasErrors())
+            return form(form.getSalaId(), form);
+
         Sessao sessao = form.toSessao(salaDao, filmeDao);
-        sessaoDao.save(sessao);
-        return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+        List<Sessao> sessoesDaSala = sessaoDao.buscaSessoesDaSala(sessao.getSala());
+        GerenciadorDeSessao gerenciador = new GerenciadorDeSessao(sessoesDaSala);
+
+        if (gerenciador.cabe(sessao)) {
+            sessaoDao.save(sessao);
+            return new ModelAndView("redirect:/admin/sala/" + form.getSalaId() + "/sessoes");
+        }
+        
+        return form(form.getSalaId(), form);
     }
 }
