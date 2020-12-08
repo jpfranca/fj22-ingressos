f0381c9726d8396c2ca5bdc98059763ce7bd651b
João França <joao.franca@outlook.com.br>
2020-12-07 21:59:12 -0300

    Exercicio 4.2

--- src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
+++ src/main/java/br/com/caelum/ingresso/dao/SessaoDao.java
@@ -1,5 +1,6 @@
 package br.com.caelum.ingresso.dao;
 
+import br.com.caelum.ingresso.model.Filme;
 import br.com.caelum.ingresso.model.Sala;
 import br.com.caelum.ingresso.model.Sessao;
 import org.springframework.stereotype.Repository;
@@ -22,6 +23,12 @@ public class SessaoDao {
         return manager.createQuery("select s from Sessao s where s.sala = :sala",
                 Sessao.class)
                 .setParameter("sala", sala)
+                .getResultList();
+    }
+
+    public List<Sessao> buscaSessoesDoFilme(Filme filme) {
+        return manager.createQuery("select s from Sessao s where s.filme = :filme", Sessao.class)
+                .setParameter("filme", filme)
                 .getResultList();
     }
 }
